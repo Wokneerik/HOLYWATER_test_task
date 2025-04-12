622955eb40f515a064412fd84be11e2f04d22ffe
Max Kireenkov <max.k@redentu.com>
2025-04-12 22:37:50 +0300

    feta: navigation from book item to Details screen

--- src/components/BookItem/index.tsx
+++ src/components/BookItem/index.tsx
@@ -1,5 +1,7 @@
+import {NavigationProp, useNavigation} from '@react-navigation/native';
 import React, {FC} from 'react';
-import {Image, Text, View} from 'react-native';
+import {Image, Text, TouchableOpacity} from 'react-native';
+import {RootStackParamList} from '../../navigation/types';
 import {Book} from '../../types';
 import styles from './styles';
 
@@ -7,12 +9,20 @@ interface BookItemProps {
   book: Book;
 }
 
+type BookItemNavigationProp = NavigationProp<RootStackParamList>;
+
 const BookItem: FC<BookItemProps> = ({book}) => {
+  const navigation = useNavigation<BookItemNavigationProp>();
+
+  const handlePress = () => {
+    navigation.navigate('Details', {bookId: book.id});
+  };
+
   return (
-    <View style={styles.bookItemContainer}>
+    <TouchableOpacity onPress={handlePress} style={styles.bookItemContainer}>
       <Image source={{uri: book.cover_url}} style={styles.bookCover} />
       <Text style={styles.bookName}>{book.name}</Text>
-    </View>
+    </TouchableOpacity>
   );
 };
 
